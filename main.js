c06442285d115fb98034b5e86dcad4945e8fff16
shen <santi.dionis@gmail.com>
2023-01-13 18:23:17 +0100

    ADD: JavaScript api 2

--- main.js
+++ main.js
@@ -5,20 +5,26 @@ encodedParams.append("target_language", "id");
 encodedParams.append("text", "What is your name?");
 
 const options = {
-	method: 'POST',
+	method: 'get',
 	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
+		//'content-type': 'application/x-www-form-urlencoded',
 		'X-RapidAPI-Key': '7b0da1adeemsh0be3fff6a70c538p13cb17jsn5b195099a61c',
 		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
 	},
-	body: encodedParams
+	//body: encodedParams
 };
 
 
-const GET_URL = 'https://text-translator2.p.rapidapi.com/translate';
+const GET_URL = 'https://text-translator2.p.rapidapi.com/getLanguages';
 
 fetch(GET_URL, options)
 .then(response => response.json())
-.then(response =>console.log(response)
-);
+.then(objeto => {
+	let lenguages = objeto.data.languages;
+	console.log()
+	//codigo de los selects
+	lenguages.forEach(element => {
+		console.log(element)
+	})
+	 })
 
