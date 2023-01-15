ec9e697c27cb740bff0c07be3da889aa1a2df595
shen <santi.dionis@gmail.com>
2023-01-15 15:44:13 +0100

    FIXED: JavaScript api POST

--- main.js
+++ main.js
@@ -16,22 +16,45 @@ const options = {
 	},
 	body: encodedParams
 };
+const OPTIONS = {
+	method: 'get',
+	headers: {
+		
+		'X-RapidAPI-Key': '7b0da1adeemsh0be3fff6a70c538p13cb17jsn5b195099a61c',
+		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+	}};
 
+const URL_GET = 'https://text-translator2.p.rapidapi.com/getLanguages';
+const URL_POST = 'https://text-translator2.p.rapidapi.com/translate';
 
-const GET_URL = 'https://text-translator2.p.rapidapi.com/translate';
-
-fetch('https://text-translator2.p.rapidapi.com/translate', options)
-.then(response => response.json())
+fetch(URL_GET, OPTIONS)
+.then(res => res.json())
 .then(objeto => {
 	let lenguages = objeto.data.languages;
+	lenguages.forEach(element => {
+		TranslateFrom.innerHTML += `<option value="${element.code}">${element.name}</option>`
+		translateTo.innerHTML += `<option value="${element.code}">${element.name}</option>`
+
+				
+			
+	})
+
+
+
+
+}
+
+
+)
+
+
+fetch(URL_POST, options)
+.then(response => response.json())
+.then(objeto => {
+
 	//console.log(objeto.data.languages[11].code)
 	//codigo de los selects
-	lenguages.forEach(element => {
 
-			TranslateFrom.innerHTML += `<option value="${element.code}">${element.name}</option>`
-			translateTo.innerHTML += `<option value="${element.code}">${element.name}</option>`
-		
-	})
 	 }).catch(err => console.log(err))
 
 	 //traduccion recoger datos textarea para enviar a la pai
